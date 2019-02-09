2cdb710867e0236c1379997cf35ef4f601c205df
mxavier91 <mxavier17@gmail.com>
2019-02-09 11:56:01 -0500

    Added a Sign Up modal to the site. When a user clicks on the "Sign-Up" link, a modal will appear, prompting the user to enter all the relevant information. Next, I want to add another modal for the "Log In" link. I also want to do more styling on the modal. Such as adding animations and curving the edges.

---
+++ app.js
@@ -0,0 +1,42 @@
+// Get Modal Element
+var modal = document.getElementById('simpleModal');
+
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+
+// Get close Button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// To target the body
+var body = document.getElementsByTagName('body')[0]
+
+
+// Listen for Open Click
+modalBtn.addEventListener('click', openModal);
+
+// Listen for Closed Click
+closeBtn.addEventListener('click', closeModal);
+
+// Listen for Outside Click
+window.addEventListener('click', clickOutside);
+
+
+// Function to Open Modal
+function openModal() {
+  modal.style.display = 'block';
+  body.style.overflow = 'hidden';
+}
+
+// Functin to Close Modal
+function closeModal() {
+  modal.style.display = 'none'
+  body.style.overflow = 'auto'
+}
+
+// Function to close modal if outside click
+function clickOutside(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none'
+    body.style.overflow = 'auto'
+  }
+}
